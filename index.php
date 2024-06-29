480d1f180c03abdb06514425b5ded261d8814d53
GuyGabriell <jonahgabriel85@yahoo.com>
2024-06-30 00:29:32 +0100

    technical check in and adding new concepts

---
+++ index.php
@@ -0,0 +1,55 @@
+
+<?php
+
+
+/*Technical check-in 001, 
+
+> adding some new concepts
+> introduce the DB
+> understanding how routing works
+>  */
+
+
+  
+$business = [
+
+'name' => 'Laracasts',
+'cost' => 15,
+'categories' => ["Testing", "PHP", "JavaScript"]
+
+];
+
+//$business['name']; // Laracasts
+
+//if ($business['cost'] > 99) {
+
+  //echo "Not interested.";
+
+//}
+
+
+//foreach ($business['categories'] as $category) {
+
+  //echo $category;
+
+  //echo $category . "<br>";
+//}
+
+
+//Now to review functions 
+
+//function register($user) {
+
+  // Create the user record in the db.
+  // Log them in.
+  // Send them a welcome email.
+  // Redirect to their new dashboard.
+
+//}
+
+
+
+require "index.view.php";
+
+
+
